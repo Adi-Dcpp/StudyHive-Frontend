679ae99899c786cf59cd151d4cb1df6b019e7b00
Adi <luffy1120adi@gmail.com>
2026-03-10 23:28:28 +0530

    feat(ui): implement global theme system with dark/light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/router'
 import { AuthProvider } from './contexts/AuthContext'
+import { ThemeProvider } from './contexts/ThemeContext.jsx'
 
 function App() {
   return (
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <ThemeProvider>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </ThemeProvider>
   )
 }
 
